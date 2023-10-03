a1dd9a127cc07b4743ad8dea3005274c947f688d
Gazi Ehsanul Haque <antik.edu@gmail.com>
2023-10-03 10:45:47 +0600

    main

---
+++ module-2/arithmatic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    //code
+    int a = 10, b = 20;
+    int sum = a+b;
+    int mins = b-a;
+    int mul = a*b;
+    int div = b/a;
+    int c = 3 ;
+    int mod = c % b;
+    double div_2 = b * 1.0/c;
+    printf("%d", mod);   
+    return 0;
+}
